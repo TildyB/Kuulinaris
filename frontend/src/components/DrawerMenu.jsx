2f06cfaea0591a9d8d3430102252f14782f2f527
MUSTANG <mustang@MUSTANGs-Mac-Pro.local>
2023-02-02 15:05:25 +0100

    css cart added

---
+++ frontend/src/components/DrawerMenu.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from "./DrawerMenu.module.css"
+
+
+const DrawerMenu = () => {
+  return (
+    <div className={styles.background}>
+    </div>
+  );
+}
+ 
+export default DrawerMenu;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,29 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./MenuBar.module.css";
 import SubMenu from "./SubMenu";
+import DrawerMenu from "./DrawerMenu";
+import { useState } from "react";
 
 const Navbar = ({ products }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const productTypes = [];
   products.map((product) => {
     if (!productTypes.includes(product.category)) {
       productTypes.push(product.category);
     }
   });
-  
+
   const newTypes = productTypes.map((type) => {
-    
     const firstLetter = type.charAt(0).toUpperCase();
     const rest = type.slice(1).toLowerCase();
     return firstLetter + rest;
   });
+
+  const openDrawer = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className={classes.nav}>
@@ -23,7 +31,10 @@ const Navbar = ({ products }) => {
         <Link to="home">Home</Link>
         <Link to="about">About</Link>
         <Link to="contact">Contact</Link>
-        <Link to='cart'>Cart</Link>
+        <Link to="cart">Cart</Link>
+        <div onClick={openDrawer}>
+          <span className="material-icons-outlined">shopping_cart</span>
+        </div>
       </div>
       <div className={classes.categories} style={{ position: "relative" }}>
         {newTypes.map((type, i) => (
@@ -32,6 +43,7 @@ const Navbar = ({ products }) => {
           </Link>
         ))}
       </div>
+      {isOpen && <DrawerMenu {...{ setIsOpen }} />}
     </nav>
   );
 };
